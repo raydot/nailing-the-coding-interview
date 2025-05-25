24d121599cca83c656e906c1da46b361e0a1d500
Dave Kanter <moadgoat@hotmail.com>
2025-05-24 17:47:33 -0700

    More work on chapter 8, coming into the homestretch on part 2

---
+++ code/ch08_hashes/collision01.py
@@ -0,0 +1,10 @@
+people_in_house = {
+    "Dad": "Bob",
+    "Mom": "Ming",
+    "Child1": "Bob", 
+    "Child2": "Sally",
+    "Child3": "Gunther",
+    "Child3": "Bartholomew"
+}
+
+console.log(people_in_house)
